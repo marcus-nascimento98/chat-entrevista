eaa6ac9a534f81106199d5f00993dc7c13523355
Auxiliar Bi <marcus_nascimento@live.com>
2025-05-22 11:19:35 -0300

    alter database

--- app/settings.py
+++ app/settings.py
@@ -77,24 +77,24 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 #DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': 'chat',
-#        'USER': 'postgres',
-#        'PASSWORD': 'postgres',
-#        'HOST': 'chat_db',
-#        'PORT': '5432',
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
 #}
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'chat',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'chat_db',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
